854619b9fde4b666c9ecc7d84ec865c1a436cfff
Rohan <adada.rohan@gmail.com>
2023-10-01 19:47:38 -0500

    deployed get_classes

--- backend/get_classes/lambda_function.py
+++ backend/get_classes/lambda_function.py
@@ -1,12 +1,18 @@
 import pymongo
 from dotenv import load_dotenv
+import json
 import os
 
 load_dotenv()
-mongodb_uri = os.getenv("MONGODB_URI")
+
+mongodb_uri = os.environ.get("MONGODB_URI")
+
 client = pymongo.MongoClient(mongodb_uri)
 db = client["schedule"]
 classes = db["classes"]
 
 def get_classes():
     return list(classes.find({}, {"_id": 0, "name": 1, "number" : 1, "code" : 1}))
+
+def lambda_handler(event, context):
+    return json.dumps(get_classes())
