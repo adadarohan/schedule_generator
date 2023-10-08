1d23f5ced1fa294688807ccd63ab1db241b5fddf
Rohan <adada.rohan@gmail.com>
2023-10-08 17:44:57 -0500

    added frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],  
+  theme: {
+    fontFamily: {
+      serif: ['Playfair Display', 'serif'],
+      display: ['Young Serif' , 'serif']
+
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
