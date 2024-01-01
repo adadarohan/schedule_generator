711a95024ab13ebe1586d757a5466301965fc44b
Rohan <adada.rohan@gmail.com>
2024-01-01 17:55:45 +0530

    added rmp

--- backend/get_sections/lambda_function.py
+++ backend/get_sections/lambda_function.py
@@ -5,6 +5,7 @@ import requests
 from datetime import datetime
 import xml.etree.ElementTree as ET  # for parsing XML
 import json
+from thefuzz import fuzz
 
 load_dotenv()
 mongodb_uri = os.environ.get("MONGODB_URI")
@@ -13,6 +14,42 @@ db = client[os.environ.get("MONGODB_DB")]
 
 classes = db["classes"]
 locations = db["locations"]
+professors = db["professors"]
+
+def get_rmp_info(first_initial, last_name, subject_code) :
+    query = {
+        'first_name' : {'$regex' : f"^{first_initial}", '$options' : "i"}, 
+        'last_name' : {'$regex' : last_name, "$options" : "i"}     
+    }
+
+    possible_profs = list(professors.find(query))
+
+    if len(possible_profs) == 0:
+        return {}
+    elif len(possible_profs) == 1 :
+        return possible_profs[0]
+    else :
+        print("More than one possible prof")
+        # Get full names of all subjects
+        resp = requests.get("https://courses.illinois.edu/cisapp/explorer/schedule/2024/spring.xml", timeout=10)
+        if resp.status_code == 404 :
+            raise Exception("Error Link not found: full class list")
+        if resp.status_code != 200 :
+            raise Exception(f"Error: {resp.status_code} for full class list")
+        
+        subjects = ET.fromstring(resp.content).find('subjects')
+        subject_dict = {ele.attrib['id'] : ele.text for ele in subjects.findall("subject")  }
+        subject_name = subject_dict.get(subject_code, '')
+
+        likely_profs = []
+        for prof in possible_profs:
+            if fuzz.token_sort_ratio(prof['department'], subject_name) > 80:
+                likely_profs.append(prof)
+
+        if len(likely_profs) == 1:
+            return likely_profs[0]
+        else : 
+            return {}
 
 def find_validate_xml (base, search) : 
     try :
@@ -22,7 +59,7 @@ def find_validate_xml (base, search) :
     
 def update_sections (clas):
 
-    resp = requests.get(clas["api_link"], timeout=10)
+    resp = requests.get(clas["api_link"] + "?mode=cascade", timeout=10)
     if resp.status_code == 404 :
         print("Error Link not found: " + clas["name"])
         raise Exception("Error Link not found: " + clas["name"])
@@ -34,20 +71,7 @@ def update_sections (clas):
     clas_root = ET.fromstring(resp.content)
     sections = []
 
-    for section in clas_root.find("sections").findall("section") :
-        resp = requests.get(section.attrib["href"], timeout=10)
-        if resp.status_code == 404 :
-            print("Error Link not found: " + section.attrib["href"])
-            continue
-        if resp.status_code != 200 :
-            print("Error: " + section.attrib["href"])
-            continue
-            
-        try :
-            section_root = ET.fromstring(resp.content)
-        except :
-            print("Error parsing XML: " + section["crn"])
-            continue
+    for section_root in clas_root.find("detailedSections").findall("detailedSection") :
         
         section_number = find_validate_xml(section_root, "sectionNumber")
         status_code = find_validate_xml(section_root, "statusCode")
@@ -82,10 +106,19 @@ def update_sections (clas):
         except :
             end_time = None
 
-        instructors = []
-        for instructor in meeting.find("instructors").findall("instructor") :
-            instructors.append(instructor.text)
+        try :
+            instructor_ele = meeting.find('instructors').find('instructor')
+            instructor = None
+            if instructor_ele is not None:
+                instructor = instructor_ele.text
+        except Exception as e :
+            instructor = None
+            print(f"Failed to get instructor with error {e}")
 
+        rmp_info = {}
+        if instructor is not None :
+            rmp_info = get_rmp_info(instructor_ele.attrib.get('firstName',''), instructor_ele.attrib.get('lastName',''), clas['code'] )
+        
         try : 
             type_code = meeting.find("type").attrib["code"]
         except :
@@ -93,14 +126,15 @@ def update_sections (clas):
         
         try :
             building_name = find_validate_xml(meeting, "buildingName")
-            coordinates = locations.find_one({"name": building_name}).get("coordinates", None)
+            coordinates = None
+            if building_name is not None :
+                coordinates = locations.find_one({"name": building_name}).get("coordinates", None)
         except Exception as e :
             print(f"Error finding coordinates for {building_name} with error {e} ")
             coordinates = None
 
         sections.append({
-            'crn': section.attrib["id"],
-            'api_link': section.attrib["href"],
+            'crn': section_root.attrib["id"],
             'section_number': section_number, 
             'status_code': status_code,
             'part_of_term': part_of_term,
@@ -118,7 +152,8 @@ def update_sections (clas):
             'room_number' : find_validate_xml(meeting, "roomNumber"),
             'building_name' : find_validate_xml(meeting, "buildingName"),
             'coordinates' : coordinates,
-            'instructors' : instructors,
+            'instructor' : instructor,
+            'rate_my_professor': rmp_info
         })
     
     if sections == [] :
@@ -134,13 +169,14 @@ def get_sections (code, number) :
     if selected_class is None :
         print("Class not found")
         return None
-    if 'last_updated' not in selected_class :
+    #elif 'last_updated' not in selected_class :
+    elif True:
         print("Updating sections")
         try : 
             return update_sections(selected_class)
         except Exception as e :
             print(f"Error updating sections with error {e}")
-    if (datetime.now() - selected_class['last_updated']).total_seconds() > 60 * 10 :
+    elif (datetime.now() - selected_class['last_updated']).total_seconds() > 60 * 10 :
         print("Updating sections")
         try : 
             return update_sections(selected_class)
